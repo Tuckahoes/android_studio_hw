d0e3e031cdf085e3ede100eb7c9e81d9ed93ca61
Tuckahoe <2510055954@qq.com>
2022-12-04 16:53:53 +0800

    added the tabLayout

--- app/src/main/java/com/example/as_hw_7/MainActivity.java
+++ app/src/main/java/com/example/as_hw_7/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.as_hw_7;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,28 +25,83 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public List<Work> workList=new ArrayList<>();
+    public List<Work> workList = new ArrayList<>();
+    private String[] tabs = {"图书", "新闻", "卖家"};
+    private List<TabFragment> tabFragmentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //init
-        for(int i=0;i<8;i++){
-            Work one_work=new Work("one_work",R.drawable.avator);
-            workList.add(one_work);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        //添加tab
+        //tabFragmentList.add(TabFragment.newInstance());
+        for (int i = 0; i < tabs.length; i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(tabs[i]));
+            tabFragmentList.add(TabFragment.newInstance(tabs[i]));
         }
-        RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        WorkAdapter adapter=new WorkAdapter(workList);
-        registerForContextMenu(recyclerView);
-        recyclerView.setLongClickable(true);
-        recyclerView.setAdapter(adapter);
+
+
+        //init
+//        for(int i=0;i<8;i++){
+//            Work one_work=new Work("one_work",R.drawable.avator);
+//            workList.add(one_work);
+//        }
+//        RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
+//        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//        WorkAdapter adapter=new WorkAdapter(workList);
+//        registerForContextMenu(recyclerView);
+//        recyclerView.setLongClickable(true);
+//        recyclerView.setAdapter(adapter);
+
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return tabFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return tabFragmentList.size();
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return tabs[position];
+            }
+        });
+
+        //设置TabLayout和ViewPager联动
+        tabLayout.setupWithViewPager(viewPager, false);
+    }
+    //bar菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.barmenu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //bar菜单新建书本事件
+            case R.id.create_book:
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+                break;
+            default:
+        }
+        return true;
     }
     //contextMenu
     @Override
@@ -71,24 +133,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onContextItemSelected(item);
     }
     //返回数据
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 1:
-                if (resultCode == RESULT_OK) {
-                    String returnData=data.getStringExtra("new_work_name");
-                    Work new_work=new Work(returnData,R.drawable.avator);
-                    workList.add(new_work);
-                    RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
-                    WorkAdapter adapter=new WorkAdapter(workList);
-                    adapter.notifyDataSetChanged();
-                    recyclerView.setAdapter(adapter);
-                }
-                break;
-            default:
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case 1:
+//                if (resultCode == RESULT_OK) {
+//                    String returnData=data.getStringExtra("new_work_name");
+//                    Work new_work=new Work(returnData,R.drawable.avator);
+//                    workList.add(new_work);
+//                    RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
+//                    WorkAdapter adapter=new WorkAdapter(workList);
+//                    adapter.notifyDataSetChanged();
+//                    recyclerView.setAdapter(adapter);
+//                }
+//                break;
+//            default:
+//        }
+//    }
+    //自定义适配器
     class WorkAdapter extends RecyclerView.Adapter<WorkAdapter.ViewHolder> {
         private List<Work> mWorkList;
 
@@ -137,7 +200,6 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-
 class Work {
     private String name;
     private int imageId;
@@ -152,3 +214,5 @@ class Work {
         return imageId;
     }
 }
+
+
